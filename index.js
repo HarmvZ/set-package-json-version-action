43cd056c8c8fe5cdfa65001c9208e7c3075587c7
HarmvZ <harm.van.zeeland@gmail.com>
2021-08-03 14:13:20 +0200

    Add input validation

--- index.js
+++ index.js
@@ -4,13 +4,17 @@ const fs = require('fs');
 
 try {
   const version = core.getInput('version');
+  if (!version) {
+    throw new Error('No version input defined');
+  }
   const inputPath = core.getInput('path');
   const packageJsonPath = resolve(inputPath, './package.json');
   const packageJson = JSON.parse(fs.readFileSync(packageJsonPath));
   const oldVersion = packageJson.version;
+  console.log(packageJson);
   packageJson.version = version;
   fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
-  console.log(`Replaced old package.json version (${packageJson.version}) with ${version}.`);
+  console.log(`Replaced old package.json version (${oldVersion}) with ${version}.`);
 } catch (error) {
   console.error(error);
   core.setFailed(error.message);
