673146829d19bc0381a22a10b664e0d8080e7492
HarmvZ <harm.van.zeeland@gmail.com>
2021-08-03 14:55:05 +0200

    Remove debug log

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ try {
   const packageJsonPath = resolve(inputPath, './package.json');
   const packageJson = JSON.parse(fs.readFileSync(packageJsonPath));
   const oldVersion = packageJson.version;
-  console.log(packageJson);
   packageJson.version = version;
   fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
   console.log(`Replaced old package.json version (${oldVersion}) with ${version}.`);
