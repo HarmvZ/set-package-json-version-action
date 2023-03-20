66a65ec93fb8b8934a7d215d2bcb1cd126cc71b0
HarmvZ <harm.van.zeeland@gmail.com>
2023-03-20 15:49:15 +0100

    Use separated function

--- index.js
+++ index.js
@@ -1,23 +1,15 @@
 const core = require('@actions/core');
-const { resolve } = require('path');
-const fs = require('fs');
-const semver = require('semver');
+const replaceVersion = require('./replaceVersion');
 
 try {
   const version = core.getInput('version');
-  const validate = core.getBooleanInput('validate');
-  if (validate && semver.valid(version) === null) {
-    throw new Error("Invalid SemVer version");
-  }
   if (!version) {
     throw new Error('No version input defined');
   }
+  const validate = core.getBooleanInput('validate');
   const inputPath = core.getInput('path');
-  const packageJsonPath = resolve(inputPath, './package.json');
-  const packageJson = JSON.parse(fs.readFileSync(packageJsonPath));
-  const oldVersion = packageJson.version;
-  packageJson.version = version;
-  fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2) + "\n");
+  
+  const oldVersion = replaceVersion(inputPath, version, validate);
   console.log(`Replaced old package.json version (${oldVersion}) with ${version}.`);
 } catch (error) {
   console.error(error);
